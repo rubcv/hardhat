5651056ccbaed3c0548b63867d66f7182b8857a5
rubcv <20502699+rubcv@users.noreply.github.com>
2024-12-05 12:54:32 +0100

    add sources and use builtin gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import 'solidity-coverage';
-import 'hardhat-gas-reporter';
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   gasReporter: {
+    enabled: true,
     currency: 'EUR',
   }
 };
